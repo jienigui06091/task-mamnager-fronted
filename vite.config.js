593805884ed488690f490ec98d6a45087e2cce65
liumanyu <706656193@qq.com>
2026-08-27 14:12:55 +0800

    修改端口号

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5273,
+    port: 18317,
     proxy: {
       '/api': {
         target: 'http://localhost:8089',
